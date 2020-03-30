524d3c98cab9eb96b6fb38a208364c4cdd84a341
Pedro Girardi <pedro@plando.work>
2020-03-30 19:00:03 +0700

    Display cells

---
+++ src/dev/clojure/user.cljs
@@ -0,0 +1,10 @@
+(ns user)
+
+(comment
+
+  (require '[literate.core :as literate])
+
+  @literate/state-ref
+  (reset! state-ref "Hello")
+
+  )
--- src/main/clojure/literate/core.cljs
+++ src/main/clojure/literate/core.cljs
@@ -1,8 +1,30 @@
 (ns literate.core
   (:require [rum.core :as rum :refer [defc]]))
 
-(defc App []
-  [:span.text-3xl "Literate"])
+(defmulti render :literate/type)
+
+(defc Code [code]
+  [:code.p-3.bg-yellow-100.rounded code])
+
+(defmethod render :literate.type/code
+  [{:literate/keys [code]}]
+  (Code code))
+
+(def state-ref (atom [#:literate {:type :literate.type/code
+                                  :code "(f x)"}
+
+                      #:literate {:type :literate.type/code
+                                  :code "(map inc [1 2 3])"}]))
+
+(defc App < rum/reactive []
+  (into [:div.flex.flex-col.p-10.rounded]
+        (for [cell (rum/react state-ref)]
+          [:div.relative.flex.bg-gray-100.p-4.mb-4
+           ;; Cell type
+           [:span.absolute.right-0.mr-3.text-xs.tracking-wider.uppercase.text-gray-500
+            (name (:literate/type cell))]
+
+           (render cell)])))
 
 (defn ^:export init []
   (rum/mount (App) (.getElementById js/document "app")))
