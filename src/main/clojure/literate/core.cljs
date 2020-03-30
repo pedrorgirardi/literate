110ad2a3b4c25ae10555f667226e7d2fc485da21
Pedro Girardi <pedro@plando.work>
2020-03-30 19:32:16 +0700

    Vega Lite literate

--- src/main/clojure/literate/core.cljs
+++ src/main/clojure/literate/core.cljs
@@ -1,20 +1,49 @@
 (ns literate.core
-  (:require [rum.core :as rum :refer [defc]]))
+  (:require [rum.core :as rum :refer [defc]]
+            ["vega-embed" :default vega-embed]))
 
 (defmulti render :literate/type)
 
 (defc Code [code]
   [:code.p-3.bg-yellow-100.rounded code])
 
+(defc VegaLite < {:did-mount (fn [state]
+                               (let [[vega-lite-spec] (:rum/args state)]
+                                 (vega-embed (rum/dom-node state) (clj->js vega-lite-spec)))
+
+                               state)}
+  [vega-lite-spec]
+  [:div])
+
 (defmethod render :literate.type/code
   [{:literate/keys [code]}]
   (Code code))
 
+(defmethod render :literate.type/vega-lite
+  [{:literate/keys [vega-lite-spec]}]
+  (VegaLite vega-lite-spec))
+
 (def state-ref (atom [#:literate {:type :literate.type/code
                                   :code "(f x)"}
 
-                      #:literate {:type :literate.type/code
-                                  :code "(map inc [1 2 3])"}]))
+                      #:literate {:type :literate.type/vega-lite
+                                  :vega-lite-spec {"$schema" "https://vega.github.io/schema/vega-lite/v2.0.json"
+                                                   :description "A simple bar chart with embedded data."
+                                                   :data {:values
+                                                          [{:a "A" :b 28}
+                                                           {:a "B" :b 55}
+                                                           {:a "C" :b 43}
+                                                           {:a "D" :b 91}
+                                                           {:a "E" :b 81}
+                                                           {:a "F" :b 53}
+                                                           {:a "G" :b 19}
+                                                           {:a "H" :b 87}
+                                                           {:a "I" :b 52}]}
+                                                   :mark "bar"
+                                                   :encoding {:x {:field "a"
+                                                                  :type "ordinal"}
+                                                              :y {:field "b"
+                                                                  :type "quantitative"}}}}]))
 
 (defc App < rum/reactive []
   (into [:div.flex.flex-col.p-10.rounded]
