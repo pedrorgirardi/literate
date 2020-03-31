494b0372d3cddd24b232a8ac60e565999be58d17
Pedro Girardi <pedro@plando.work>
2020-03-31 09:25:39 +0700

    Color tweaks

--- src/main/clojure/literate/core.cljs
+++ src/main/clojure/literate/core.cljs
@@ -48,12 +48,12 @@
 (defc App < rum/reactive []
   (into [:div.flex.flex-col.p-10.rounded
 
-         [:span.text-2xl.text-yellow-600.mb-10
+         [:span.text-2xl.mb-10
           {:style {:font-family "Cinzel"}}
           "Literate"]]
 
         (for [cell (rum/react state-ref)]
-          [:div.relative.flex.bg-gray-100.p-10.mb-10.shadow
+          [:div.relative.flex.bg-white.p-10.mb-10.shadow
            ;; Cell type
            [:span.absolute.top-0.mt-3.right-0.mr-3.text-xs.tracking-wider.uppercase.text-gray-500
             (name (:literate/type cell))]
