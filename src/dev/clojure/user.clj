a96c598c38cd52d87b36d9ac8597137296a804b0
Pedro Girardi <pedro@plando.work>
2021-01-05 14:59:46 +0700

    Update example

--- src/dev/clojure/user.clj
+++ src/dev/clojure/user.clj
@@ -112,20 +112,12 @@
        (l/vega-lite-snippet
          {"$schema" "https://vega.github.io/schema/vega-lite/v4.json"
           :description "A simple bar chart with embedded data."
-          :data {:values
-                 [{:a "A" :b 28}
-                  {:a "B" :b 55}
-                  {:a "C" :b 43}
-                  {:a "D" :b 91}
-                  {:a "E" :b 81}
-                  {:a "F" :b 53}
-                  {:a "G" :b 19}
-                  {:a "H" :b 87}
-                  {:a "I" :b 52}]}
-          :mark "bar"
-          :encoding {:x {:field "a"
-                         :type "ordinal"}
-                     :y {:field "b"
+          :data {:url "https://vega.github.io/editor/data/stocks.csv"}
+          :transform [{"filter" "datum.symbol==='GOOG'"}],
+          :mark "line"
+          :encoding {:x {:field "date"
+                         :type "temporal"}
+                     :y {:field "price"
                          :type "quantitative"}}})
 
        ;; -- Code
