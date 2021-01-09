520e5252feb358a55425e5afcd89fdaf5d91ce65
Pedro Girardi <pedro@plando.work>
2021-01-09 08:39:05 +0700

    Update client

--- src/dev/clojure/user.clj
+++ src/dev/clojure/user.clj
@@ -15,7 +15,7 @@
 
 (defn start-server [& [{:keys [port]}]]
   (alter-var-root #'stop-server (fn [_]
-                                  (server/run-server {:port (or port 8080)}))))
+                                  (server/run-server {:port (or port 8118)}))))
 
 (defn reset []
   (stop-server)
@@ -31,38 +31,38 @@
   (http/post "http://localhost:8080/api/v1/transact" {:body (server/transit-encode [(literate/code "Hello")])})
 
 
+  (def l (partial literate/view {:url "http://localhost:8118"}))
+
+
   ;; -- Vega Lite.
 
-  (literate/view
-    (literate/vega-lite
-      {"$schema" "https://vega.github.io/schema/vega-lite/v4.json"
-       :description "A simple bar chart with embedded data."
-       :data {:values
-              [{:a "A" :b 28}
-               {:a "B" :b 55}
-               {:a "C" :b 43}
-               {:a "D" :b 91}
-               {:a "E" :b 81}
-               {:a "F" :b 53}
-               {:a "G" :b 19}
-               {:a "H" :b 87}
-               {:a "I" :b 52}]}
-       :mark "bar"
-       :encoding {:x {:field "a"
-                      :type "ordinal"}
-                  :y {:field "b"
-                      :type "quantitative"}}}))
+  (l (literate/vega-lite
+       {"$schema" "https://vega.github.io/schema/vega-lite/v4.json"
+        :description "A simple bar chart with embedded data."
+        :data {:values
+               [{:a "A" :b 28}
+                {:a "B" :b 55}
+                {:a "C" :b 43}
+                {:a "D" :b 91}
+                {:a "E" :b 81}
+                {:a "F" :b 53}
+                {:a "G" :b 19}
+                {:a "H" :b 87}
+                {:a "I" :b 52}]}
+        :mark "bar"
+        :encoding {:x {:field "a"
+                       :type "ordinal"}
+                   :y {:field "b"
+                       :type "quantitative"}}}))
 
   ;; -- Code.
 
-  (literate/view
-    (literate/code (slurp (io/resource "literate/core.clj"))))
+  (l (literate/code (slurp (io/resource "literate/core.clj"))))
 
 
   ;; -- Leaflet.
 
-  (literate/view
-    (literate/leaflet {}))
+  (l (literate/leaflet {}))
 
   (def geojson (json/read (io/reader "src/dev/resources/points.geojson")))
 
@@ -77,117 +77,111 @@
        :zoom 10
        :geojson (update geojson "features" #(take 10 %))}))
 
-  (literate/view leaflet-widget)
+  (l leaflet-widget)
 
-  (literate/view
-    (merge leaflet-widget {:widget/geojson (update geojson "features" #(take 1 %))}))
+  (l (merge leaflet-widget {:widget/geojson (update geojson "features" #(take 1 %))}))
 
 
   ;; -- Markdown.
 
-  (literate/view
-    (literate/markdown "**Welcome to Literate**\n\nEval some forms to get started!"))
+  (l (literate/markdown "**Welcome to Literate**\n\nEval some forms to get started!"))
 
 
   ;; -- HTML.
 
-  (literate/view
-    (literate/html
-      (rum.server-render/render-static-markup
-        [:div.bg-white.p-3
-         [:h1.text-6xl "Hello from Hiccup"]
-         [:span "Text"]])))
+  (l (literate/html
+       (rum.server-render/render-static-markup
+         [:div.bg-white.p-3
+          [:h1.text-6xl "Hello from Hiccup"]
+          [:span "Text"]])))
 
 
   ;; -- Row.
 
-  (literate/view
-    (literate/row
-      {}
-      (literate/leaflet {})
-      (literate/vega-lite
-        {"$schema" "https://vega.github.io/schema/vega-lite/v4.json"
-         :description "A simple bar chart with embedded data."
-         :data {:url "https://vega.github.io/editor/data/stocks.csv"}
-         :transform [{"filter" "datum.symbol==='GOOG'"}],
-         :mark "line"
-         :encoding {:x {:field "date"
-                        :type "temporal"}
-                    :y {:field "price"
-                        :type "quantitative"}}})))
+  (l (literate/row
+       {}
+       (literate/leaflet {})
+       (literate/vega-lite
+         {"$schema" "https://vega.github.io/schema/vega-lite/v4.json"
+          :description "A simple bar chart with embedded data."
+          :data {:url "https://vega.github.io/editor/data/stocks.csv"}
+          :transform [{"filter" "datum.symbol==='GOOG'"}],
+          :mark "line"
+          :encoding {:x {:field "date"
+                         :type "temporal"}
+                     :y {:field "price"
+                         :type "quantitative"}}})))
 
 
   ;; -- Column.
 
-  (literate/view
-    (literate/column
-      {}
-      (literate/leaflet {})
-      (literate/vega-lite
-        {"$schema" "https://vega.github.io/schema/vega-lite/v4.json"
-         :description "A simple bar chart with embedded data."
-         :data {:url "https://vega.github.io/editor/data/stocks.csv"}
-         :transform [{"filter" "datum.symbol==='GOOG'"}],
-         :mark "line"
-         :encoding {:x {:field "date"
-                        :type "temporal"}
-                    :y {:field "price"
-                        :type "quantitative"}}})))
+  (l (literate/column
+       {}
+       (literate/leaflet {})
+       (literate/vega-lite
+         {"$schema" "https://vega.github.io/schema/vega-lite/v4.json"
+          :description "A simple bar chart with embedded data."
+          :data {:url "https://vega.github.io/editor/data/stocks.csv"}
+          :transform [{"filter" "datum.symbol==='GOOG'"}],
+          :mark "line"
+          :encoding {:x {:field "date"
+                         :type "temporal"}
+                     :y {:field "price"
+                         :type "quantitative"}}})))
 
 
   ;; -- Welcome.
 
-  (literate/view
-    (literate/column
-      {}
-      (literate/hiccup
-        [:div.flex.flex-col.space-y-3.bg-white.p-3.font-light
-         [:h1.text-3xl
-          {:style {:font-family "Cinzel"}}
-          "Welcome to Literate"]
+  (l (literate/column
+       {}
+       (literate/hiccup
+         [:div.flex.flex-col.space-y-3.bg-white.p-3.font-light
+          [:h1.text-3xl
+           {:style {:font-family "Cinzel"}}
+           "Welcome to Literate"]
 
-         [:p.font-semibold
-          "Literate is a Clojure & ClojureScript application which you can use to visualize data."]
+          [:p.font-semibold
+           "Literate is a Clojure & ClojureScript application which you can use to visualize data."]
 
-         [:p.mt-4
-          "This interface that you're looking at it's called a " [:span.font-bold "Widget"]
-          ", and you can create one from a Clojure REPL."]
+          [:p.mt-4
+           "This interface that you're looking at it's called a " [:span.font-bold "Widget"]
+           ", and you can create one from a Clojure REPL."]
 
-         [:p.mt-2.mb1 "There are a few different types of Widgets that are supported:"]
+          [:p.mt-2.mb1 "There are a few different types of Widgets that are supported:"]
 
-         [:ul.list-disc.list-inside.ml-2
-          [:li "Code"]
-          [:li "Markdown"]
-          [:li "Hiccup"]
-          [:li "Vega Lite"]
-          [:li "Leaflet"]
-          [:li "Column layout"]
-          [:li "Row layout"]]])
+          [:ul.list-disc.list-inside.ml-2
+           [:li "Code"]
+           [:li "Markdown"]
+           [:li "Hiccup"]
+           [:li "Vega Lite"]
+           [:li "Leaflet"]
+           [:li "Column layout"]
+           [:li "Row layout"]]])
 
-      (literate/hiccup
-        [:span.p-2.text-lg "Vega Lite Widget"])
+       (literate/hiccup
+         [:span.p-2.text-lg "Vega Lite Widget"])
 
-      (literate/vega-lite
-        {"$schema" "https://vega.github.io/schema/vega-lite/v4.json"
-         :description "A simple bar chart with embedded data."
-         :data {:url "https://vega.github.io/editor/data/stocks.csv"}
-         :transform [{"filter" "datum.symbol==='GOOG'"}],
-         :mark "line"
-         :encoding {:x {:field "date"
-                        :type "temporal"}
-                    :y {:field "price"
-                        :type "quantitative"}}})
+       (literate/vega-lite
+         {"$schema" "https://vega.github.io/schema/vega-lite/v4.json"
+          :description "A simple bar chart with embedded data."
+          :data {:url "https://vega.github.io/editor/data/stocks.csv"}
+          :transform [{"filter" "datum.symbol==='GOOG'"}],
+          :mark "line"
+          :encoding {:x {:field "date"
+                         :type "temporal"}
+                     :y {:field "price"
+                         :type "quantitative"}}})
 
-      (literate/hiccup
-        [:span.p-2.text-lg "Code Widget"])
+       (literate/hiccup
+         [:span.p-2.text-lg "Code Widget"])
 
-      (literate/code (slurp (io/resource "literate/core.clj")))
+       (literate/code (slurp (io/resource "literate/core.clj")))
 
-      (literate/hiccup
-        [:span.p-2.text-lg "Leaflet Widget"])
+       (literate/hiccup
+         [:span.p-2.text-lg "Leaflet Widget"])
 
-      (literate/leaflet {:style {:height "400px"}
-                         :center [51.505 -0.09]
-                         :zoom 10})))
+       (literate/leaflet {:style {:height "400px"}
+                          :center [51.505 -0.09]
+                          :zoom 10})))
 
   )
