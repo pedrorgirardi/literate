43d8fe6d1e0591f8192d9f69db3ef8911f9bdc66
Pedro Girardi <pedro@plando.work>
2021-01-09 08:03:04 +0700

    CLI options

--- src/main/clojure/literate/core.clj
+++ src/main/clojure/literate/core.clj
@@ -1,5 +1,22 @@
 (ns literate.core
-  (:require [literate.server :as server]))
+  (:require [literate.server :as server]
+            [clojure.tools.cli :as cli]))
+
+(def cli-options
+  [["-p" "--port PORT" "Port number"
+    :default 8118
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]])
 
 (defn -main [& args]
-  (server/run-server {:port 8080}))
+  (let [opts (cli/parse-opts args cli-options)
+        port (get-in opts [:options :port])]
+
+    (println
+      (str "Welcome to Literate\n"
+           "Starting server..."))
+
+    (server/run-server {:port port})
+
+    (println "Up and running on port:" port)
+    (println "Happy coding!")))
