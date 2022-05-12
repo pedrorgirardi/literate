608b424fa08f5110b7905614a8e49613b7975fe2
Pedro Girardi <prg@fastmail.com>
2022-05-12 08:59:28 -0300

    Tool and site

--- src/main/clojure/literate/core.clj
+++ src/main/clojure/literate/core.clj
@@ -8,15 +8,17 @@
     :parse-fn #(Integer/parseInt %)
     :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]])
 
+(defn start [{:keys [port]}]
+  (println
+    (str "Welcome to Literate\n"
+      "Starting server..."))
+
+  (server/run-server {:port port})
+
+  (println "Server is up and running on port:" port)
+  (println "Happy coding!"))
+
 (defn -main [& args]
   (let [opts (cli/parse-opts args cli-options)
         port (get-in opts [:options :port])]
-
-    (println
-      (str "Welcome to Literate\n"
-           "Starting server..."))
-
-    (server/run-server {:port port})
-
-    (println "Server is up and running on port:" port)
-    (println "Happy coding!")))
+    (start {:port port})))
